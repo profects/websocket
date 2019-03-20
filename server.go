7e22d940f1a6fed988bb18964dfb78142c6189e8
Rubenvdveen <ruben.van.der.veen@profects.com>
2019-03-20 15:15:31 +0100

    Update server.go

--- server.go
+++ server.go
@@ -123,7 +123,7 @@ func (u *Upgrader) selectSubprotocol(r *http.Request, responseHeader http.Header
 func (u *Upgrader) Upgrade(w http.ResponseWriter, r *http.Request, responseHeader http.Header) (*Conn, error) {
 	const badHandshake = "websocket: the client is not using the websocket protocol: "
 
-	if !tokenListContainsValue(r.Header, "Connection", "upgrade") {
+	if !tokenListContainsValue(r.Header, "Connection", "Upgrade") {
 		return u.returnError(w, r, http.StatusBadRequest, badHandshake+"'upgrade' token not found in 'Connection' header")
 	}
 
